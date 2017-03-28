5be2caf27df7805ddcbb7a0e710d2c282e18cfbd
phillyai <sonagi.philly@gmail.com>
2017-03-28 16:53:47 +0900

    에러 수정

--- main.py
+++ main.py
@@ -5,22 +5,16 @@ from time import sleep
 
 class Infre:
     def __init__(self):
-        self.driver = webdriver.PhantomJS('D://chromedriver.exe')
-        self.driver.set_window_position(0, 0)
-        self.driver.set_window_size(0, 0)
+        self.driver = webdriver.Chrome('D://chromedriver.exe')
         pass
 
     def __enter__(self):
-        pass
+        return self
 
-    def __exit__(self, **kwargs):
+    def __exit__(self, *args):
         self.driver.quit()
 
-    def dispose(self):
-        self.__exit__()
-
     def join(self, s_id, pw, name, gender, msg):
-        print('join')
         self.driver.get('http://infre.kr/Account/join.php')
         f_id = self.driver.find_element_by_id('join_user_id')
         f_pw = self.driver.find_element_by_id('join_user_pw')
@@ -46,7 +40,15 @@ class Infre:
         self.driver.switch_to.alert.accept()
         # self.driver.execute_script('window.confirm = function(msg) { return true; }')
 
+    def login(self, s_id, pw):
+        self.driver.get('http://infre.kr/Account/login.php')
+        self.driver.find_element_by_id('user_id').clear()
+        self.driver.find_element_by_id('user_id').send_keys(s_id)
+        self.driver.find_element_by_id('user_pw').clear()
+        self.driver.find_element_by_id('user_pw').send_keys(pw)
+        self.driver.execute_script('loginSubmit();')
+
 if __name__ == '__main__':
-    i = Infre()
-    i.join('00112', '1234', '안뇽', 'm', '메시지')
-    i.dispose()
+    with Infre() as i:
+        i.join('00112', '1234', '안뇽', 'm', '메시지')
+        i.login('00112', '1234')
